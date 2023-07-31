73f5af515c6d26cfae570a241cbd496a9d4fd492
chapcs <benchapman003@gmail.com>
2023-07-31 13:04:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class TallGuy
+{
+    public string Name;
+    public int Height;
+
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine($"My name is {Name} and I'm {Height} inches tall");
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+        tallGuy.TalkAboutYourself();
+    }
+}
